66d8cf5fd820f891eeb1a35c59bf00c8ff894de8
Jay_1213 <arun.jay23@vit.edu>
2026-03-25 19:26:33 +0530

    Refine server header removal in security middleware

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -90,7 +90,8 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
             response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
 
         # Remove server identification headers
-        response.headers.pop("server", None)
+        if "server" in response.headers:
+            del response.headers["server"]
 
         return response
 
